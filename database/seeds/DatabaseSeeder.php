2fd6c6e7251d57246ef935882f1c596b88a07161
Milan Bancevic <m.bancevic@gmail.com>
2020-03-20 17:49:21 +0100

    zadatak1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(MoviesTableSeeder::class); // odkomentarises i promenis UsersTableSeeder u svoj ts
     }
 }
